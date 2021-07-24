f5f47ac47440190e80a4208c3b0c71d7f844ae22
CristianSC <cristiansk8x@gmail.com>
2021-07-24 19:23:29 -0300

    Enemy Get Damage

--- SFML_Test/Enemy.cpp
+++ SFML_Test/Enemy.cpp
@@ -1,7 +1,18 @@
 #include "pch.h"
 #include "Enemy.h"
+#include "Bullet.h"
 
 Enemy::Enemy(ColliderManager* colliderManager, GameObject* owner, CharacterInput* charInput, float speed) : Player(colliderManager, owner, charInput, speed)
 {
 	textureComponent->Tint(sf::Color::Magenta);
+}
+
+void Enemy::OnTriggerEnter(GameObject* go)
+{
+	Bullet* b = (Bullet*)go->GetComponent("Bullet");
+	if (b != nullptr) 
+	{
+		HPComponent->TakeDamage(2);
+		cout << HPComponent->GetCurrentHP() << endl;
+	}
 }
--- SFML_Test/Enemy.h
+++ SFML_Test/Enemy.h
@@ -5,5 +5,7 @@ class Enemy : public Player
 {
 public:
 	Enemy(ColliderManager* colliderManager, GameObject* owner, CharacterInput* charInput, float speed);
+	void OnTriggerEnter(GameObject* go);
+
 };
 
